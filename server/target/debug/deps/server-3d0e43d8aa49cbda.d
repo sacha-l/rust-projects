e44f55c9567dc1b06478e71f8b0a3a94b34d069e
Sacha Lansky <sacha@parity.io>
2022-04-17 14:03:33 +0200

    Add build

--- server/target/debug/deps/server-3d0e43d8aa49cbda.d
+++ server/target/debug/deps/server-3d0e43d8aa49cbda.d
@@ -1,5 +1,9 @@
-/Users/sachalansky/Projects/Rust_Course/server/target/debug/deps/server-3d0e43d8aa49cbda: src/main.rs
+/Users/sachalansky/Projects/Rust_Course/server/target/debug/deps/server-3d0e43d8aa49cbda: src/main.rs src/server.rs src/http/mod.rs src/http/method.rs src/http/request.rs
 
-/Users/sachalansky/Projects/Rust_Course/server/target/debug/deps/server-3d0e43d8aa49cbda.d: src/main.rs
+/Users/sachalansky/Projects/Rust_Course/server/target/debug/deps/server-3d0e43d8aa49cbda.d: src/main.rs src/server.rs src/http/mod.rs src/http/method.rs src/http/request.rs
 
 src/main.rs:
+src/server.rs:
+src/http/mod.rs:
+src/http/method.rs:
+src/http/request.rs:
--- server/target/debug/server.d
+++ server/target/debug/server.d
@@ -1,1 +1,1 @@
-/Users/sachalansky/Projects/Rust_Course/server/target/debug/server: /Users/sachalansky/Projects/Rust_Course/server/src/main.rs
+/Users/sachalansky/Projects/Rust_Course/server/target/debug/server: /Users/sachalansky/Projects/Rust_Course/server/src/http/method.rs /Users/sachalansky/Projects/Rust_Course/server/src/http/mod.rs /Users/sachalansky/Projects/Rust_Course/server/src/http/request.rs /Users/sachalansky/Projects/Rust_Course/server/src/main.rs /Users/sachalansky/Projects/Rust_Course/server/src/server.rs
