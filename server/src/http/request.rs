d24b73fac80c9fbdd0dd353eb0e9037e66c9c5d5
Sacha Lansky <sacha@parity.io>
2022-04-17 14:04:17 +0200

    Add new server stuff

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,7 +1,24 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
+}
+
+// impl Request {
+//     fn from_byte_array(buf: &[u8]) -> Result<Self, String> {}
+// }
+
+// implement some trait on the byte array type
+// we need to pass in a concrete type 
+impl TryFrom<&[u8]> for Request {
+
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        // we can use the unimplemented!() macro if we don't want to implement this
+        unimplemented!();
+    }
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,3 +1,8 @@
+use std::net::TcpListener;
+use std::io::Read;
+// go to the root of a crate
+use crate::http::Request;
+
 // we have to state what in this mod is public
 // what data needs to be associated 
 pub struct Server {
@@ -15,5 +20,43 @@ impl Server {
     // run will take ownership of Server
     pub fn run(self) {
         println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        
+        // accept a connection
+        // if accept fails we don't want the program to terminate
+        loop {
+            // much better than let (stream, addr) = res.unwrap();
+            match listener.accept() {
+
+                // get the tuple result and println
+                Ok((mut stream, _)) => {
+                    //stream.read(&mut )
+                    // we need to store a large number of bytes
+                    let mut buffer = [0; 1024];
+                    // what if the data is larger ?
+                    match stream.read(&mut buffer) {
+
+                        Ok(_) => {
+                            // log the request we receive
+                            // convert the bytes to string
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+
+                },
+                //match the error case
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+        }
+
     }
 }
+
+// if you dont know the size of an array, pass in a reference to some 
+// array and the compiler will know how to handle any number of elements
+// e.g. a = [1, 2, 3];
+// arr(&a[1..]);
+
+// tests: echo "TEST" | netcat 127.0.0.1 8080
