a8a0cdbd1b22efba39cf623bf8d79cee87f3c0cd
Sacha Lansky <sacha@parity.io>
2022-04-24 15:51:46 +0200

    Add read_file method to WebsiteHandler

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -1,5 +1,6 @@
 use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
+use std::fs;
 
 // allow handler to take a path to read HTML files
 pub struct WebsiteHandler {
@@ -10,6 +11,12 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        // this returns a string so we need to convert it to an Option using .ok()
+        fs::read_to_string(path).ok()
+    }
 }
 
 impl Handler for WebsiteHandler {
@@ -18,8 +25,8 @@ impl Handler for WebsiteHandler {
         // handle all possible requests
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
                 _ => Response::new(StatusCode::NotFound, None),
             }
             _ => Response::new(StatusCode::NotFound, None),
